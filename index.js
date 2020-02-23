31ddcf2a6040827f71c624cd736ee9f4957003eb
ivopavlov <pavlov.ivo@gmail.com>
2020-02-23 15:57:36 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+const db = require('./config/keys').mongoURI
+
+mongoose
+  .connect(db, { useNewURLParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello, World!asdfasdf'));
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
