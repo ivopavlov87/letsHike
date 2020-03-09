f4e2d839392145686e3bb983a32d1631c9a702c7
ivopavlov <pavlov.ivo@gmail.com>
2020-03-09 12:13:32 -0700

    DC'd GMap API key

--- frontend/src/components/hikes/hike_create.jsx
+++ frontend/src/components/hikes/hike_create.jsx
@@ -159,7 +159,7 @@ class HikeCompose extends React.Component {
     if (this.props.hike){
       theMap = (
         <Map
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`} //&key=${process.env.REACT_APP_GOOGLE_KEY}`}
           loadingElement={<div style={{ height: "100%" }} />}
           containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
